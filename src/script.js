1aa54457578ca5d6f1e86d90ebabcf7575368def
Caio Imperio <ImperioCaioo@gmail.com>
2023-12-18 09:53:47 -0300

    Projeito calculadora feito

--- src/script.js
+++ src/script.js
@@ -2,7 +2,41 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //ESCREVA SEU CÓDIGO AQUI!!
+function Calculadora(numero1,numero2,operador){
 
+let num1 = parseInt(numero1)
+let num2 = parseInt(numero2)
+let resultado
+
+switch (operador){
+  case "+":
+    resultado = num1 + num2
+    break
+  case "-":
+    resultado = num1 - num2
+    break
+  case "/":
+    resultado = num1 / num2
+    break
+  case "*":
+    resultado = num1 * num2
+    break
+  case "e":
+    resultado = num1 * num1
+    let contador = 1
+    while (contador < num2 - 1){
+      resultado = resultado * num1
+      contador++;
+    }
+    break
+}
+
+if (resultado === undefined || resultado > 1000000){
+
+   resultado = "Erro"
+}
+return resultado
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
